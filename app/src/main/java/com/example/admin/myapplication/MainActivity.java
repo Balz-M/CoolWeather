5cb9b899c794802186222b503608738719c4117a
admin <1064693056@qq.com>
2019-04-10 09:18:11 +0800

    coolweather4.10

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.admin.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -12,14 +15,20 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView view
-            ;
-
+    private TextView view;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.view = (TextView) findViewById(R.id.abc);
+        this.button=(Button)findViewById(R.id.btn);
+        this.button.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public  void onClick(View v){
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+            }
+        });
         String wweatherId = "CN101270104";
         String weatherUrl = "http://guolin.tech/api/weather?cityid=" + wweatherId + "&key=f2f20935bd70455b83730a9973dadf54";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
